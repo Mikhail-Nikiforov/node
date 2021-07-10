1e64a8d451a126c4f3eba56665a4f890625bdc71
Mikhail <nickiforov.mihail2017@yandex.ru>
2021-07-10 14:14:05 +0300

    Edit

--- index.js
+++ index.js
@@ -13,11 +13,6 @@ readStream.on('data', (chunk) => {
                 const writeStream = fs.createWriteStream(`./${addresses[i]}_request.log`, { flags: 'a', encoding: 'utf8' });
                 writeStream.write(element + `\n`);
                 writeStream.end();
-
-            } else if (element.includes(addresses[i])) {
-                const writeStream = fs.createWriteStream(`./${addresses[i]}_request.log`, { flags: 'a', encoding: 'utf8' });
-                writeStream.write(element + `\n`);
-                writeStream.end();
             }
         }
     });
@@ -27,26 +22,3 @@ readStream.on('data', (chunk) => {
 
 readStream.on('end', () => console.log('File reading finished'));
 readStream.on('error', (err) => console.log(err));
-
-// writeStream.on('error', err => console.log(err));
-
-
-// readStream.pipe(writeStream).pipe(process.stdout);
-
-// const log1 = '89.123.1.41 - - [30/Jan/2021:11:11:20 -0300] "POST /foo HTTP/1.1" 200 0 "-" "curl/7.47.0"';
-// const log2 = '34.48.240.111 - - [30/Jan/2021:11:11:25 -0300] "GET /boo HTTP/1.1" 404 0 "-" "curl/7.47.0"';
-
-// fs.writeFile('./access.log', log1, {flag: 'a' }, (err) => console.log(err));
-// fs.writeFile('./access.log', '\n', { flag: 'a' }, (err) => console.log(err));
-
-// readStream.on('data', (chunk) => {
-//     console.log('chunk');
-//     console.log(chunk);
-// });
-// readStream.on('end', () => console.log('File reading finished'));
-// readStream.on('error', () => console.log(err));
-
-// writeStream.write('\n');
-// writeStream.write(log1);
-
-// writeStream.end(() => console.log('File writing finished'));

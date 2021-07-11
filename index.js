3cba4241ae1cc1d92e716c2528ed3358fd4a9ae3
Mikhail <nickiforov.mihail2017@yandex.ru>
2021-07-11 13:46:12 +0300

    Edit

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const fs = require('fs');
-const readStream = fs.createReadStream('./access.log', 'utf8');
 
+
+const readStream = fs.createReadStream('./access.log', 'utf8');
+const adresses = [`89.123.1.41`, `34.48.240.111`];
+const writeStreams = [];
+
+adresses.forEach(element => {
+    writeStreams.push({ ip: element, item: fs.createWriteStream(`./${element}_request.log`, { flags: 'a', encoding: 'utf8' }) });
+});
 
 readStream.on('data', (chunk) => {
-    let arr = chunk.split('\n');
-    let addresses = [`89.123.1.41`, `34.48.240.111`];
-    
+    let arr = chunk.split('\n');    
 
     arr.forEach(element => {
-        for (let i = 0; i < addresses.length; i++) {
-            if (element.includes(addresses[i])) {
-                const writeStream = fs.createWriteStream(`./${addresses[i]}_request.log`, { flags: 'a', encoding: 'utf8' });
-                writeStream.write(element + `\n`);
-                writeStream.end();
+        for (let i = 0; i < writeStreams.length; i++) {
+            if (element.includes(writeStreams[i].ip)) {
+                writeStreams[i].item.write(element + `\n`);
             }
         }
     });
-    
-    console.log(arr);
 });
 
-readStream.on('end', () => console.log('File reading finished'));
+
+readStream.on('end', () => {
+    console.log('Чтение файла заверешено');
+    writeStreams.forEach(element => {
+    element.item.on('finish', () => {
+        console.log(`Файл с логами для IP ${element.ip} создан`);
+    });
+    element.item.end();
+});
+});
 readStream.on('error', (err) => console.log(err));

3420170cbf71ee6a7f5cca3f7db4cbae401e04a3
Mikhail <nickiforov.mihail2017@yandex.ru>
2021-07-14 10:26:07 +0300

    HW4 done

--- index.js
+++ index.js
@@ -1,34 +1,75 @@
+#!/usr/bin/env node
+
+const EventEmmitter = require('events');
 const fs = require('fs');
+const yargs = require("yargs");
+const path = require("path");
+const readline = require("readline");
+const inquirer = require("inquirer");
 
-
-const readStream = fs.createReadStream('./access.log', 'utf8');
-const adresses = [`89.123.1.41`, `34.48.240.111`];
-const writeStreams = [];
-
-adresses.forEach(element => {
-    writeStreams.push({ ip: element, item: fs.createWriteStream(`./${element}_request.log`, { flags: 'a', encoding: 'utf8' }) });
+const emitter = new EventEmmitter();
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
 
-readStream.on('data', (chunk) => {
-    let arr = chunk.split('\n');    
+emitter.on(`pathChosen`, (directoryPath) => {
+    let list = fs.readdirSync(directoryPath)
 
-    arr.forEach(element => {
-        for (let i = 0; i < writeStreams.length; i++) {
-            if (element.includes(writeStreams[i].ip)) {
-                writeStreams[i].item.write(element + `\n`);
+    inquirer
+        .prompt([
+            {
+                name: "fileName",
+                type: "list",
+                message: "Choose file:",
+                choices: list,
+            },
+        ])
+        .then((answer) => {
+            const filePath = path.join(directoryPath, answer.fileName);
+            if (fs.lstatSync(filePath).isFile()) {
+                fs.readFile(filePath, 'utf8', (err, data) => {
+                    if (err) throw err;
+                    emitter.emit(`fileChosen`, data);
+                });
+                
+            } else {
+                emitter.emit(`pathChosen`, filePath);
             }
-        }
+        })
+        .catch((err) => console.log(err));
+});
+emitter.on(`fileChosen`, (fileData) => {
+
+    let dataArr = fileData.split(`\n`);
+    rl.resume();
+    rl.question("Укажите IP-адреса (если их несколько, укажите через пробел): ", (adresses) => {
+        emitter.emit(`ipChosen`, adresses, dataArr);
+        
     });
 });
+emitter.on(`ipChosen`, (ipArr, dataArr) => {
+    if (ipArr.includes(` `)) {
+        ipArr = ipArr.split(` `);
+    } else {
+        ipArr = [ipArr];
+    }
 
+    for (let i = 0; i < dataArr.length; i++) {
+        ipArr.forEach(element => {
+            if (dataArr[i].includes(element)) {
+                fs.writeFileSync(`./${element}_request.log`, dataArr[i] + `\n`, { encoding: "utf8", flag: "a+" })
+            }
+        });
+    }
 
-readStream.on('end', () => {
-    console.log('Чтение файла заверешено');
-    writeStreams.forEach(element => {
-    element.item.on('finish', () => {
-        console.log(`Файл с логами для IP ${element.ip} создан`);
-    });
-    element.item.end();
+    console.log(`Логи отфильтрованы`);
+    process.exit();
 });
+
+rl.question("Введите путь к директории содержащей файл: ", (inputedPath) => {
+    let directoryPath = path.join(process.cwd(), inputedPath);
+    rl.pause();
+    emitter.emit(`pathChosen`, directoryPath);
+    
 });
-readStream.on('error', (err) => console.log(err));
